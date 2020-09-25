6113e2dde00281ce257adefbe404730f615c6057
blackymax <maxmin3007@gmail.com>
2020-09-25 20:11:48 +0300

    feat: add task solution for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let arr1 =  [];
+    let str
+    let arr = String(n).split('');
+    if (arr.includes('-')){
+        arr.shift()
+    }
+    arr.reverse()
+    arr.forEach(function(item, index) {
+    arr1.push(arr[index])
+    str = arr1.join('')
+    
+    })
+    return str
 }
